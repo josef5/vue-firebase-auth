5de61fbc9fd5fd3022da297f73b59a0040b6b836
Jose Espejo <joseespejo@yahoo.com>
2024-03-31 19:08:00 +0100

    feat: add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
-import "./style.css";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+import "./style.css";
 
-createApp(App).use(router).mount("#app");
+const pinia = createPinia();
+
+createApp(App).use(pinia).use(router).mount("#app");
