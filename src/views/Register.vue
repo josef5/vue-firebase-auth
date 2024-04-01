dd77e98d960c980e3bfa10db2c231d7b1d9ecf77
Jose Espejo <joseespejo@yahoo.com>
2024-04-01 18:31:51 +0100

    feat: add login components

---
+++ src/views/Register.vue
@@ -0,0 +1,91 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { useStoreAuth } from "../stores/storeAuth";
+import { useRoute, useRouter } from "vue-router";
+
+const storeAuth = useStoreAuth();
+const route = useRoute();
+const router = useRouter();
+
+const email = ref("");
+const password = ref("");
+const errorMessages = ref<string[]>([]);
+
+const validateForm = () => {
+  errorMessages.value = [];
+
+  if (email.value === "" || password.value === "") {
+    errorMessages.value.push("Both fields are required.");
+  }
+
+  if (!email.value.includes("@")) {
+    errorMessages.value.push("Email address must be a valid email.");
+  }
+
+  if (password.value.length < 6) {
+    errorMessages.value.push("Password must be at least 6 characters.");
+  }
+
+  if (errorMessages.value.length > 0) {
+    return false;
+  }
+
+  return true;
+};
+
+const handleSubmit = async () => {
+  if (validateForm()) {
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+
+    try {
+      await storeAuth.register({
+        email: email.value,
+        password: password.value,
+      });
+
+      // If a redirect route is provided in the query parameters, use it. Otherwise, default to the home route
+      let redirectRoute = route.query.redirect;
+      if (Array.isArray(redirectRoute)) {
+        redirectRoute = redirectRoute[0];
+      }
+      router.push(redirectRoute || "/");
+    } catch (error) {
+      console.error(error);
+      errorMessages.value = [(error as Error).message];
+    }
+  }
+};
+</script>
+
+<template>
+  <h1>Register</h1>
+
+  <form @submit.prevent="handleSubmit">
+    <label for="email">Email</label>
+    <input
+      type="text"
+      id="email"
+      name="email"
+      placeholder="e.g. alice@example.com"
+      v-model="email"
+    />
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password" v-model="password" />
+    <button type="submit">Register</button>
+  </form>
+  <p v-for="errorMessage in errorMessages">{{ errorMessage }}</p>
+  <small>Already registered? <RouterLink to="/login">Login</RouterLink></small>
+</template>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  width: 300px;
+
+  button {
+    margin-block: 1rem;
+  }
+}
+</style>
