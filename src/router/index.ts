520fece5355a97b60e1046017f0daa40a4ac55bd
Jose Espejo <joseespejo@yahoo.com>
2024-04-01 18:32:26 +0100

    feat: set auth routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Admin from "../views/Admin.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import { useStoreAuth } from "../stores/storeAuth";
 
 const routes = [
   {
@@ -12,6 +15,17 @@ const routes = [
     path: "/admin",
     name: "admin",
     component: Admin,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register,
   },
 ];
 
@@ -20,13 +34,17 @@ const router = createRouter({
   routes,
 });
 
-const isAuthed = false;
-
 // Protecting routes
 router.beforeEach(async (to) => {
-  // If the user is not authenticated and tries to access the admin route, they will be redirected to the home route
-  if (to.name === "admin" && !isAuthed) {
-    return { name: "home" };
+  // If the user is not authenticated and the route requires authentication then redirect to login
+  // Set the query parameter redirect to the current route name
+  const storeAuth = useStoreAuth();
+
+  if (to.meta.requiresAuth && !storeAuth.user.id) {
+    return {
+      name: "login",
+      query: { redirect: to.name ? to.name.toString() : undefined },
+    };
   }
 });
 
